ee5dc0c912bb3b582053874f88c6ccb6d3c72763
chenfukye <kye1234321@gmail.com>
2024-08-08 23:49:39 +0800

    [ADD] add docker compose and script

---
+++ src/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /opt/ros/humble/setup.bash
+
+colcon build 
+
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
+ros2 run pros_yolo soccer_node &
+ros2 run pros_soccer car_control
+
+# 防止腳本結束導致容器退出
+wait
