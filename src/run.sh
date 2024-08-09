9b8c8c711b8521f57efb778673e294d4dc83d409
chenfukye <kye1234321@gmail.com>
2024-08-09 11:51:53 +0800

    [FIX] fix run.sh can not use pros_soccer node

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,7 @@
 source /opt/ros/humble/setup.bash
 
 colcon build 
+source /workspaces/src/install/setup.bash
 
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
 ros2 run pros_yolo soccer_node &
